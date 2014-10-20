08eb535bb591d8fc069133542838f13683a5f649
Mohamed GUEMBRI <mohamedguembri@Mr-Gmbios.local>
2014-10-20 23:24:17 +0100

    Initial Commit

---
+++ UIGitHub/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  UIGitHub
+//
+//  Created by Mohamed GUEMBRI on 20/10/2014.
+//  Copyright (c) 2014 Mohamed GUEMBRI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIGitHub/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  UIGitHub
+//
+//  Created by Mohamed GUEMBRI on 20/10/2014.
+//  Copyright (c) 2014 Mohamed GUEMBRI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
